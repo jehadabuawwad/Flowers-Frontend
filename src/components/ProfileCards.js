02197081c7c09f510d492dd7665655f4049d422f
jehadabuawwad <jehadabuawwad@outlook.com>
2021-09-26 16:01:10 +0300

    The Repo Setup Finished

---
+++ src/components/ProfileCards.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProfileCards extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
